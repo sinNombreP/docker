05a29e040ebf6492649fc526037d6fd8c1fc85a2
wmzr <psz199616@gmail.com>
2019-06-03 23:22:05 +0800

    first commit

---
+++ src/main/java/com/wmzr/docker/api/demoCollltor.java
@@ -0,0 +1,23 @@
+package com.wmzr.docker.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+/**
+ * @author 彭善智
+ * @Title: demoCollltor
+ * @ProjectName docker
+ * @Description: TODO
+ * @date 2019/6/323:16
+ */
+@RestController
+public class demoCollltor {
+
+    @RequestMapping("hello")
+    public Date hello(){
+        return  new Date();
+    }
+
+}
